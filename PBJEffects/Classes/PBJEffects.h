0e56abb3fc72348fd5f67654f7db2da47be6a20b
Joseph Razon <razonjoe@gmail.com>
2018-02-19 11:04:04 +0200

    Wrong function header fix.

--- PBJEffects/Classes/PBJEffects.h
+++ PBJEffects/Classes/PBJEffects.h
@@ -33,7 +33,7 @@
 #pragma mark Blur
 
 
-+(UIView *)addBlurToView:(UIView *)view;
++(UIView *)addBlurToView:(UIView *)view withStyle:(UIBlurEffectStyle)style;
 +(UIImage *)addBlurToImage:(UIImage *)image;
 
 
